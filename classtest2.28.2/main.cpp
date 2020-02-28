45342bdbf8a3729a53d57c2ead2b903864e4cc0d
jiayi guo <1351361929@qq.com>
2020-02-28 14:52:43 +0800

    更新cpp

---
+++ classtest2.28.2/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<opencv2\opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\1.jpg",0);
+	/*int height = srcMat.rows; //行数 
+	int width = srcMat.cols; //每行元素的总元素数量 
+	for (int j = 0; j<height; j++) {
+		for (int i = 0; i<width; i++) {
+			//-----------------开始处理每个像素----------------
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+			//-------------结束像素处理-----------------------
+		} //单行处理结束 
+	}*/
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
+
+}
