0cd9f184e9ba729ca1540f3c2e7453fbfad98192
plussone <gjy1351361929@gmail.com>
2020-03-27 14:46:33 +0800

    0327练习四

--- Digital-image-processing/032704.cpp
+++ Digital-image-processing/032704.cpp
@@ -12,8 +12,15 @@ void test032704()
 	{
 		Mat frame;
 		Mat newMat;
+		Mat grad_x, grad_y;
+		Mat abs_grad_x, abs_grad_y;
+
 		cap >> frame;
-		Sobel(frame, newMat, -1, 1, 1);
+		Sobel(frame, grad_x, CV_16S, 1, 0);
+		convertScaleAbs(grad_x, abs_grad_x);
+		Sobel(frame, grad_y, CV_16S, 0, 1);
+		convertScaleAbs(grad_y, abs_grad_y);
+		addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, newMat);
 		imshow("frame", frame);
 		imshow("newMat", newMat);
 
